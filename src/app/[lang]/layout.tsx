75d3ab0ce35dbc01ff96d9e652332dc113fd7f4d
Vikto0004 <vikto0004@gmail.com>
2024-09-09 09:58:31 +0200

    hide Header

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -8,7 +8,7 @@ import { getMessages } from "next-intl/server";
 
 import "./globals.css";
 import "react-toastify/ReactToastify.min.css";
-import Header from "../../core/Header/Header";
+// import Header from "../../core/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,7 +33,7 @@ export default async function RootLayout({
     <html lang={locale}>
       <body className={inter.className}>
         <NextIntlClientProvider messages={messages}>
-          <Header />
+          {/* <Header /> */}
           {children}
         </NextIntlClientProvider>
         <ToastContainer />
