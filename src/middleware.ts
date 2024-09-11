966250559089e3fcb4321a995bac13a90d332672
Vikto0004 <vikto0004@gmail.com>
2024-09-11 11:05:03 +0200

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-// import { chain } from "@/middlewares/chain";
-// import { withAuthMiddleware } from "@/middlewares/withAuthMiddleware";
-// import { withI18nMiddleware } from "@/middlewares/withI18nMiddleware";
+import { chain } from "@/middlewares/chain";
+import { withAuthMiddleware } from "@/middlewares/withAuthMiddleware";
+import { withI18nMiddleware } from "@/middlewares/withI18nMiddleware";
 
-// export default chain([withAuthMiddleware, withI18nMiddleware]);
-
-// export const config = {
-//   matcher: ["/((?!_next).*)"],
-// };
-
-import createMiddleware from "next-intl/middleware";
-
-import { routing } from "./i18n/routing";
-
-export default createMiddleware(routing);
+export default chain([withAuthMiddleware, withI18nMiddleware]);
 
 export const config = {
-  matcher: ["/", "/(ua|en)/:path*"],
+  matcher: ["/((?!_next).*)"],
 };
+
+// import createMiddleware from "next-intl/middleware";
+
+// import { routing } from "./i18n/routing";
+
+// export default createMiddleware(routing);
+
+// export const config = {
+//   matcher: ["/", "/(ua|en)/:path*"],
+// };
