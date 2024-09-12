987c4617b16c5b34efef9f71317ebe9c0b2a6063
Vikto0004 <vikto0004@gmail.com>
2024-09-12 10:55:07 +0200

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,3 @@
-// import { chain } from "@/middlewares/chain";
-// import { withAuthMiddleware } from "@/middlewares/withAuthMiddleware";
-// import { withI18nMiddleware } from "@/middlewares/withI18nMiddleware";
-
-// export default chain([withAuthMiddleware, withI18nMiddleware]);
-
-// export const config = {
-//   matcher: ["/((?!_next).*)"],
-// };
-
 import createMiddleware from "next-intl/middleware";
 
 import { routing } from "./i18n/routing";
