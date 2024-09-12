a521f2670cc99144bf1230c03fd907bcbc9f470a
Vikto0004 <vikto0004@gmail.com>
2024-09-12 10:52:52 +0200

    Update Container.tsx

--- src/core/Container.tsx
+++ src/core/Container.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import SectionContainer from "./SectionContainer";
+import SectionContainer from "./SectionContainer/SectionContainer";
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
