0cc44051262b7babf92dbc61b57d1c1ac464d035
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-09-13 19:20:48 +0300

    news upd

---
+++ public/Images/image.ts
@@ -0,0 +1,11 @@
+import cardImg1 from "./CardNews1.webp";
+import cardImg2 from "./CardNews2.webp";
+import cardImg3 from "./CardNews3.webp";
+
+export const cardImages = {
+  cardImg1,
+  cardImg2,
+  cardImg3,
+};
+
+export default cardImages;
--- src/core/News/News.tsx
+++ src/core/News/News.tsx
@@ -1,33 +1,37 @@
+import Image from "next/image";
+import { FiArrowUpRight } from "react-icons/fi";
 import CustomButton from "../CustomButton/CustomButton";
 import SectionContainer from "../SectionContainer/SectionContainer";
-import style from "./News.module.css";
 import ua from "../../../messages/ua.json";
-import { FiArrowUpRight } from "react-icons/fi";
-import Image from "next/image";
+import cardImages from "../../../public/Images/image";
+import style from "./News.module.css";
+import { oldStandardTT, montserrat } from "../fonts";
 
 const News = () => {
   return (
     <SectionContainer>
       <div className={style.wrapper}>
-        <h2 className={style.title}>{ua.News.title}</h2>
+        <h2 className={`${oldStandardTT.className} ${style.title}`}>
+          {ua.News.title}
+        </h2>
         <ul className={style.list}>
           {ua.News.card.map((card) => {
             return (
               <li key={card.cardId} className={style.listItem}>
                 <a href={"/"} className={style.linkCard}>
-                  {/* <Image
-                    src="/public/Images/CardNews1.png"
-                    width={400}
-                    height={500}
+                  <Image
+                    src={cardImages.cardImg1}
                     alt="Card background image"
                     loading="lazy"
-                  /> */}
+                  />
                   <div className={style.contentCard}>
                     <div className={style.contentWrap}>
                       <div className={style.upRightArrowWrap}>
                         <FiArrowUpRight className={style.upRightArrow} />
                       </div>
-                      <div className={style.titleInfoWrap}>
+                      <div
+                        className={`${montserrat.className} ${style.titleInfoWrap}`}
+                      >
                         <h3 className={style.cardTitle}>{card.cardTitle}</h3>
                         <p className={style.data}>{card.publicationData}</p>
                         <p className={style.fondName}>{card.fondName}</p>
