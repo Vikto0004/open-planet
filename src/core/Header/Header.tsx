b9793acc3f4591860dbcea1eb2bf483f5953bf11
Vikto0004 <vikto0004@gmail.com>
2024-09-18 17:07:17 +0200

    change searchInput and fixed bags

--- src/core/Header/Header.tsx
+++ src/core/Header/Header.tsx
@@ -13,45 +13,71 @@ import SocIcons from "../SocIcons/SocIcons";
 
 import { montserrat } from "../fonts";
 import css from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState("");
+
   const { lang } = useParams();
   const translate = useTranslations("Header");
+
+  const handleSearch = (): void => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query: string = e.target.value;
+    setValue(query);
+  };
 
   return (
     <header className={css.header}>
       <div className={css.container}>
         <Logo />
-        <nav className={css.navigate}>
-          <NavLink
-            href={`/${lang}`}
-            styles={`${montserrat.className} ${css.link}`}
-          >
-            {translate("home")}
-          </NavLink>
-          <PopoverList />
-          <NavLink
-            href={`/${lang}/lignes-of-work`}
-            styles={`${montserrat.className} ${css.link}`}
-          >
-            {translate("directionsWork")}
-          </NavLink>
-          <NavLink
-            href={`/${lang}/news`}
-            styles={`${montserrat.className} ${css.link}`}
-          >
-            {translate("news")}
-          </NavLink>
-          <NavLink
-            href={`/${lang}/reports`}
-            styles={`${montserrat.className} ${css.link}`}
-          >
-            {translate("reports")}
-          </NavLink>
-        </nav>
         <div className={css.wrapper}>
+          <nav className={css.navigate}>
+            {!isOpen && (
+              <>
+                <NavLink
+                  href={`/${lang}`}
+                  styles={`${montserrat.className} ${css.link}`}
+                >
+                  {translate("home")}
+                </NavLink>
+                <PopoverList />
+                <NavLink
+                  href={`/${lang}/lignes-of-work`}
+                  styles={`${montserrat.className} ${css.link}`}
+                >
+                  {translate("directionsWork")}
+                </NavLink>
+                <NavLink
+                  href={`/${lang}/news`}
+                  styles={`${montserrat.className} ${css.link}`}
+                >
+                  {translate("news")}
+                </NavLink>
+                <NavLink
+                  href={`/${lang}/reports`}
+                  styles={`${montserrat.className} ${css.link}`}
+                >
+                  {translate("reports")}
+                </NavLink>
+              </>
+            )}
+            {isOpen && (
+              <input
+                type="text"
+                className={`${montserrat.className} ${css.input}`}
+                placeholder={translate("search")}
+                value={value}
+                onChange={handleChange}
+              />
+            )}
+          </nav>
           <div className={css.wrap}>
-            <SearchInput />
+            <SearchInput handleSearch={handleSearch} />
             <SelectLang />
           </div>
           <SocIcons />
--- src/core/SearchInput/SearchInput.tsx
+++ src/core/SearchInput/SearchInput.tsx
@@ -1,48 +1,11 @@
-import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import { useSpring, animated } from "@react-spring/web";
 
 import css from "./SearchInput.module.css";
-import { montserrat } from "../fonts";
-import { useTranslations } from "next-intl";
 
-const SearchInput = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = useState("");
-  const translate = useTranslations("Header");
-
-  const props = useSpring({
-    width: isOpen ? "600px" : "0px",
-    opacity: isOpen ? 1 : 0,
-    padding: isOpen ? "17px" : "0px",
-    config: { duration: 300 },
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const query: string = e.target.value;
-    setValue(query);
-  };
-
+const SearchInput = ({ handleSearch }: { handleSearch: () => void }) => {
   return (
     <>
-      {isOpen && (
-        <div onClick={() => setIsOpen(!isOpen)} className={css.overlay}></div>
-      )}
-      <div className={css.wrap}>
-        <animated.input
-          type="text"
-          placeholder={translate("search")}
-          value={value}
-          className={`${montserrat.className} ${css.inputSearch}`}
-          style={{ ...props }}
-          onChange={handleChange}
-        />
-        <FiSearch
-          onClick={() => setIsOpen(!isOpen)}
-          size="24px"
-          className={css.searchIcon}
-        />
-      </div>
+      <FiSearch onClick={handleSearch} size="24px" className={css.searchIcon} />
     </>
   );
 };
