e26059046e0332cde0874a4eee9f6239f40e7a72
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2024-09-24 11:06:20 +0300

    fix home

--- src/app/api/[lang]/route.ts
+++ src/app/api/[lang]/route.ts
@@ -7,9 +7,8 @@ import { getDatafromToken } from "@/services/tokenServices";
 
 import { handleRoutesError } from "@/errors/errorRoutesHandler";
 import { errorHandler } from "@/errors/errorHandler";
-import { WorkDirectionsModel } from "@/models/workDirections-model";
 import { NewsModel } from "@/models/news-model";
-import { QuestionModel } from "@/models/questions-model";
+
 
 
 connect();
@@ -18,11 +17,10 @@ export async function GET(req: NextRequest) {
   try {
     const pathName = req.nextUrl.pathname.split("/")[2];
 
-    await WorkDirectionsModel.find();
     await NewsModel.find();
-    await QuestionModel.find();
 
-    const homeData = await HomeModel.findOne({ language: pathName }).populate({ path: "workDirections", select: "header description url" }).populate({ path: "questions", select: "header description url" }).populate({ path: "news" });
+
+    const homeData = await HomeModel.findOne({ language: pathName }).populate({ path: "news" }).exec();
 
     return NextResponse.json({ homeData });
   } catch (error: unknown) {
@@ -34,16 +32,3 @@ export async function GET(req: NextRequest) {
   }
 }
 
-export async function POST(req: NextRequest) {
-  try {
-    const userData = getDatafromToken(req);
-    if (userData?.role !== "admin") throw errorHandler("Forbidden", 403);
-    const reqBody = await req.json();
-
-    const res = await HomeModel.create(reqBody);
-
-    return NextResponse.json({ res });
-  } catch (error: unknown) {
-    return handleRoutesError(error);
-  }
-}
