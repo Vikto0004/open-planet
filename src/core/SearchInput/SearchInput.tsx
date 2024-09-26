ea8e02882fc71ac272929a221e7225a0d9d00a8a
Vikto0004 <vikto0004@gmail.com>
2024-09-26 16:57:15 +0200

    changed searchInput on better

--- src/core/SearchInput/SearchInput.tsx
+++ src/core/SearchInput/SearchInput.tsx
@@ -1,11 +1,34 @@
-import { FiSearch } from "react-icons/fi";
+import { useTranslations } from "next-intl";
+import { useState } from "react";
+
+import { montserrat } from "../fonts";
 
 import css from "./SearchInput.module.css";
 
-const SearchInput = ({ handleSearch }: { handleSearch: () => void }) => {
+type PropsType = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SearchInput = ({ setIsOpen, isOpen }: PropsType) => {
+  const translate = useTranslations("Header");
+  const [value, setValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query: string = e.target.value;
+    setValue(query);
+  };
+
   return (
     <>
-      <FiSearch onClick={handleSearch} size="24px" className={css.searchIcon} />
+      <div className={css.overlay} onClick={() => setIsOpen(!isOpen)}></div>
+      <input
+        type="text"
+        className={`${montserrat.className} ${css.input}`}
+        placeholder={translate("search")}
+        value={value}
+        onChange={handleChange}
+      />
     </>
   );
 };
