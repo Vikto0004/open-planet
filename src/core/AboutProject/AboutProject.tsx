b4f0380e76437d5ef34a2505cb2f5d1b4fee02cb
Vikto0004 <vikto0004@gmail.com>
2024-10-09 18:38:53 +0200

    added translation, AboutProjectImagesList in AboutProject

--- src/core/AboutProject/AboutProject.tsx
+++ src/core/AboutProject/AboutProject.tsx
@@ -1,21 +1,32 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+
+import { isValidLang } from "@/utils/helper";
 import { support } from "@/utils/routes";
 
 import calendarIcon from "../../../public/svgs/calendar.svg";
+import AboutProjectImagesList from "../AboutProjectImagesList/AboutProjectImagesList";
 import Container from "../Container/Container";
 import CustomButton from "../CustomButton/CustomButton";
 import { inter, montserrat, oldStandardTT } from "../fonts";
 import Section from "../Section/Section";
+import Title from "../Title/Title";
 
 import css from "./AboutProject.module.css";
+import { financProject, firstDiscr, secondSubDiscr } from "./dataBombShelter";
 
 export default function AboutProject() {
+  const translate = useTranslations("AboutProject");
+  const { lang } = useParams();
+
   return (
     <Section style={css.section}>
       <Container>
         <h1 className={`${oldStandardTT.className} ${css.title}`}>
-          Реконструкція бомбосховища для Школи №25
+          {translate("firstTitle")}
         </h1>
         <div className={css.dataWrapper}>
           <Image
@@ -26,7 +37,7 @@ export default function AboutProject() {
             loading="lazy"
           />
           <p className={`${inter.className} ${css.publicationData}`}>
-            28 серпня 2024
+            {translate("date")}
           </p>
         </div>
         <Image
@@ -37,91 +48,61 @@ export default function AboutProject() {
           className={css.mainImg}
         />
         <ul className={css.list}>
-          <li className={`${montserrat.className} ${css.listItem}`}>
-            Школа №25 була побудована у 1939 році за рекордні 3 місяці. Ця
-            історична будівля знаходиться у центрі міста, біля знаменитої
-            Андріївської церкви та особливою не тільки для працівників школи, а
-            й для учнів та батьків. На сьогоднішній день у нашому навчальному
-            закладі освіту здобуває 569 учні.
-          </li>
-          <li className={`${montserrat.className} ${css.listItem}`}>
-            Наразі, у звʼязку з воєнним станом та постійними ракетними атаками,
-            діти вимушені майже кожного дня спускатись до укриття. Як укриття
-            використовується підвальне приміщення, яке не було спочатку
-            призначене для цих цілей, і його поточний стан не відповідає жодним
-            стандартам і не дозволяє дітям перебувати там хоча б у якихось
-            мінімально комфортних умовах.
-          </li>
-          <li className={`${montserrat.className} ${css.listItem}`}>
-            На жаль, держава не може в повному обсязі профінансувати капітальний
-            ремонт приміщень. Безпека дітей є пріорітетною для Школи, тому
-            керівництво звертається за підтримкою до всіх небайдужих.
-          </li>
+          {firstDiscr[isValidLang(lang)].map((text, index) => (
+            <li
+              key={index}
+              className={`${montserrat.className} ${css.listItem}`}
+            >
+              {text}
+            </li>
+          ))}
         </ul>
+        <Title text={translate("secondTitle")} style={css.secondTitle} />
+        <AboutProjectImagesList />
         <div className={css.wrap}>
           <h3 className={`${oldStandardTT.className} ${css.subtitle}`}>
-            Мета проєкту (І етап) :
+            {translate("firstSubtitle")}
           </h3>
           <p className={`${montserrat.className} ${css.listItem}`}>
-            Переобладнати старе підвальне приміщення школи в сучасне укриття,
-            щоб усі діти та вчителі мали змогу безпечно перебувати там під час
-            повітряних тривог.
+            {translate("firstSubDiscr")}
           </p>
           <h3 className={`${oldStandardTT.className} ${css.subtitle}`}>
-            Бюджет та необхілне фінансування (І етап)
+            {translate("secondSubtitle")}
           </h3>
           <ul className={css.listFinanc}>
-            <li className={css.listFinancItem}>
-              <p
-                className={`${oldStandardTT.className} ${css.listFinancDiscr}`}
-              >
-                Капітальний ремонт укриття за проєктом
-              </p>
-              <p className={`${montserrat.className} ${css.listFinancTitle}`}>
-                4,055, 528 грн
-              </p>
-            </li>
-            <li className={css.listFinancItem}>
-              <p
-                className={`${oldStandardTT.className} ${css.listFinancDiscr}`}
-              >
-                Виділено коштів
-              </p>
-              <p className={`${montserrat.className} ${css.listFinancTitle}`}>
-                1, 583, 543 грн
-              </p>
-            </li>
-            <li className={css.listFinancItem}>
-              <p
-                className={`${oldStandardTT.className} ${css.listFinancDiscr}`}
-              >
-                Не вистачає коштів
-              </p>
-              <p className={`${montserrat.className} ${css.listFinancTitle}`}>
-                2, 392, 242 грн
-              </p>
-            </li>
+            {financProject[isValidLang(lang)].map(({ id, discr, money }) => {
+              return (
+                <li key={id} className={css.listFinancItem}>
+                  <p
+                    className={`${oldStandardTT.className} ${css.listFinancDiscr}`}
+                  >
+                    {discr}
+                  </p>
+                  <p
+                    className={`${montserrat.className} ${css.listFinancTitle}`}
+                  >
+                    {money}
+                  </p>
+                </li>
+              );
+            })}
           </ul>
           <ul className={css.listDisrc}>
-            <li className={`${montserrat.className} ${css.listItem}`}>
-              Наразі виконуються роботи з встановлення двох санітарних вузлів,
-              системи вентиляції та шпаклювання 30% стін. Не вистачає коштів на
-              заміну підлоги, шпаклювання 70% стін, на встановлення вхідних
-              дверей в укриття, нової системи опалення та оновлення стелі.
-            </li>
-            <li className={`${montserrat.className} ${css.listItem}`}>
-              У цей складний і невизначений час безпека наших дітей стає
-              пріоритетом номер один. Ми звертаємося до вас із важливою
-              ініціативою — створення сучасного і безпечного укриття для дітей.
-              Це не просто інвестиція в інфраструктуру, це внесок у майбутнє
-              нашої нації.
-            </li>
-            <li className={`${montserrat.className} ${css.listItem}`}>
-              Щиро вдячні за вашу увагу та сподіваємось на вашу підтримку.
-            </li>
+            {secondSubDiscr[isValidLang(lang)].map((text, index) => (
+              <li
+                key={index}
+                className={`${montserrat.className} ${css.listItem}`}
+              >
+                {text}
+              </li>
+            ))}
           </ul>
         </div>
-        <CustomButton style={css.button} text="Підтримати" link={support} />
+        <CustomButton
+          style={css.button}
+          text={translate("button")}
+          link={support}
+        />
       </Container>
     </Section>
   );
