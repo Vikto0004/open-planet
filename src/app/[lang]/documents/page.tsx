5f33e397daf4edf6e26794b725f01d6e4a204e11
Vikto0004 <vikto0004@gmail.com>
2024-10-10 13:07:18 +0200

    add pagination

---
+++ src/app/[lang]/documents/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/core/Container/Container";
+import Section from "@/core/Section/Section";
+
+export default function Documents() {
+  return (
+    <Section>
+      <Container>
+        <h1>Welcome to Documents page</h1>
+      </Container>
+    </Section>
+  );
+}
