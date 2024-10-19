13a079ce527437fc9bc24fdbd2088f67781f9ad9
Vikto0004 <vikto0004@gmail.com>
2024-10-19 22:10:47 +0200

    CooperationList mobil - done

--- src/core/About/About.tsx
+++ src/core/About/About.tsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 
 import AboutList from "../AboutList/AboutList";
 import Container from "../Container/Container";
-// import CooperationList from "../CooperationList/CooperationList";
+import CooperationList from "../CooperationList/CooperationList";
 import { montserrat, oldStandardTT } from "../fonts";
 import Section from "../Section/Section";
 import Title from "../Title/Title";
@@ -31,7 +31,7 @@ export default function About() {
           <h3 className={`${oldStandardTT.className} ${css.titleOurValues}`}>
             {translate("cooperation")}
           </h3>
-          {/* <CooperationList /> */}
+          <CooperationList />
         </Container>
       </Section>
     </>
