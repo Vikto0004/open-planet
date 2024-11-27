4f32d8fc100fd0681ea6d4895b485bf4509b4fd1
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-11-27 13:36:22 +0100

    pagination-commit

--- src/core/CardsLigneWorkPaginate/CardsLigneWorkPaginate.tsx
+++ src/core/CardsLigneWorkPaginate/CardsLigneWorkPaginate.tsx
@@ -1,6 +1,10 @@
 import clsx from "clsx";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
+import { FiArrowUpRight } from "react-icons/fi";
 import { LuArrowLeft, LuArrowRight } from "react-icons/lu";
 import ReactPaginate from "react-paginate";
+import { useMediaQuery } from "react-responsive";
 
 import { oldStandardTT } from "../fonts";
 
@@ -19,17 +23,40 @@ export default function CardsLigneWorkPaginate({
     setCurrentPage(selectedItem.selected + 1);
   };
 
+  const isMobile = useMediaQuery({ query: "(max-width: 1240px)" });
+  const translate = useTranslations("paginateLoadMoreButton");
+  const [isClient, setisClient] = useState(false);
+
+  useEffect(() => {
+    setisClient(true);
+  }, []);
+
   return (
     <ReactPaginate
       previousLabel={
-        <button className={css.button}>
-          <LuArrowLeft size={30} />
-        </button>
+        isMobile ? (
+          <button className={css.buttonMobileHidden}>
+            {translate("buttonText")}
+          </button>
+        ) : (
+          <button className={css.button}>
+            <LuArrowLeft size={30} />
+          </button>
+        )
       }
       nextLabel={
-        <button className={css.button}>
-          <LuArrowRight size={30} />
-        </button>
+        isMobile ? (
+          <div className={css.buttonMobileContainer}>
+            <button className={css.buttonMobile}>
+              {translate("buttonText")}
+              <FiArrowUpRight size="25px" className={css.icon} />
+            </button>
+          </div>
+        ) : (
+          <button className={css.button}>
+            <LuArrowRight size={30} />
+          </button>
+        )
       }
       breakLabel={"..."}
       pageCount={totalPages}
