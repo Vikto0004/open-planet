bc2c7b594e135bd7b9f7a709bf1970c301df1b13
Vikto0004 <vikto0004@gmail.com>
2024-11-29 20:57:53 +0100

    Update page.tsx

--- src/app/[lang]/public-offer/page.tsx
+++ src/app/[lang]/public-offer/page.tsx
@@ -1,9 +1,11 @@
+import FAQ from "@/core/FAQ/FAQ";
 import PublicOffer from "@/core/PublicOffer/PublicOffer";
 
 export default async function PublicOfferPage() {
   return (
     <>
       <PublicOffer />
+      <FAQ />
     </>
   );
 }
