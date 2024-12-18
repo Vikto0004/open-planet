f8b7b3b1e73abb9679edf993d0ddb2ba46b8266c
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-12-18 08:05:33 +0100

    programs-commit

--- src/core/CardsLigneWorkList/CardsLigneWorkList.tsx
+++ src/core/CardsLigneWorkList/CardsLigneWorkList.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { useMediaQuery } from "react-responsive";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { useEffect, useState } from "react";
+
 import { Project } from "@/query/types/projects";
 import { useSelectedWork } from "@/utils/hooks";
 
@@ -16,22 +22,49 @@ export default function CardsLigneWorkList({
   projects,
   programType,
 }: PropsType) {
+  const isMobile = useMediaQuery({ query: "(max-width: 1023px)" });
+  const [isClient, setIsClient] = useState(false);
+  const [pathname, setPathname] = useState("");
+
+  useEffect(() => {
+    setIsClient(true);
+    setPathname(window.location.pathname);
+  }, []);
+
   const selectedWork = useSelectedWork(programType);
 
-  return (
-    <ul className={css.list}>
-      {projects.map((obj) => {
-        return (
+  if (!isClient) {
+    return null;
+  }
+
+  const isProgramsPath = pathname.endsWith("/programs");
+
+  return isProgramsPath && isMobile ? (
+    <Swiper spaceBetween={20} slidesPerView="auto" grabCursor={true}>
+      {projects.map((obj) => (
+        <SwiperSlide key={obj._id} className={css.swiperSlide}>
           <CardsLigneWorkItem
-            key={obj._id}
             content={{
               obj,
               programType,
               selectedWork,
             }}
           />
-        );
-      })}
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  ) : (
+    <ul className={css.list}>
+      {projects.map((obj) => (
+        <CardsLigneWorkItem
+          key={obj._id}
+          content={{
+            obj,
+            programType,
+            selectedWork,
+          }}
+        />
+      ))}
     </ul>
   );
 }
