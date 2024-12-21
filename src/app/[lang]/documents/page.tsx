0a97f502a84730fb409117bb0227a77b4d42e1c4
Oleksandr Karnaushenko <oleksandr.karnaushenko@gmail.com>
2024-12-21 17:54:14 +0100

    Create doc and rep components
    create documents component
    create reports component
    add css from mobile
    create local json file with testing data
    add testing pdf file

--- src/app/[lang]/documents/page.tsx
+++ src/app/[lang]/documents/page.tsx
@@ -1,9 +1,11 @@
-import PageUnderDevelopment from "@/core/PageUnderDevelopment/PageUnderDevelopment";
+import DocumentsHome from "@/core/Documents/Documents";
+import FAQ from "@/core/FAQ/FAQ";
 
 export default function Documents() {
   return (
     <>
-      <PageUnderDevelopment />
+      <DocumentsHome />
+      <FAQ />
     </>
   );
 }
---
+++ src/core/DocRepCard/DocRepCard.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import React, { useState, useEffect } from "react";
+
+import styles from "./DocRepCard.module.css";
+
+interface Card {
+  cardTitle: string;
+  publicationData: string;
+  link: string;
+}
+
+type Props = {
+  card: Card;
+};
+
+export default function DocRepCard({ card }: Props) {
+  const translate = useTranslations("Documents");
+  const { cardTitle, publicationData, link } = card;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const verifyDevice = () => {
+      if (window.innerWidth < 1240) {
+        setIsMobile(true); // mobile devices
+      } else {
+        setIsMobile(false); // desktop
+      }
+    };
+
+    verifyDevice();
+
+    window.addEventListener("resize", verifyDevice);
+    return () => {
+      window.removeEventListener("resize", verifyDevice);
+    };
+  }, []);
+
+  const handleDownload = () => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = link;
+    downloadLink.download = `${cardTitle}.pdf`;
+    downloadLink.click();
+  };
+
+  return (
+    <>
+      <div className={styles.card}>
+        <p>
+          {isMobile
+            ? publicationData
+            : `${translate("published")}: ${publicationData}`}
+        </p>
+        <h3 className={styles.title}>{cardTitle}</h3>
+        <button className={styles.btn} onClick={handleDownload}>
+          <Image
+            src="/svgs/generate-pdf.svg"
+            alt="Download"
+            width={32}
+            height={32}
+            className={styles.icon}
+          />
+          {!isMobile && translate("btnName")}
+        </button>
+      </div>
+    </>
+  );
+}
