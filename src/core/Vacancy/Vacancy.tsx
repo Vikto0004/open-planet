081157aa0023fd45cc7dc102df34ba7d8e05c2bc
Vikto0004 <vikto0004@gmail.com>
2025-01-18 15:32:02 +0100

    fixed

--- src/core/Vacancy/Vacancy.tsx
+++ src/core/Vacancy/Vacancy.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
 import { BiShoppingBag } from "react-icons/bi";
 import { GrLocation } from "react-icons/gr";
 import { IoEllipse } from "react-icons/io5";
@@ -5,6 +8,7 @@ import { IoEllipse } from "react-icons/io5";
 import vacancy from "@/db-local/vacancy.json";
 import { useValidLang } from "@/utils/hooks";
 
+import calendarIcon from "../../../public/svgs/calendar.svg";
 import Container from "../Container/Container";
 import Renderer from "../Renderer/Renderer";
 import Section from "../Section/Section";
@@ -14,16 +18,32 @@ import css from "./Vacancy.module.css";
 
 export default function Vacancy() {
   const lang = useValidLang();
+  const translate = useTranslations("JoinUs");
 
   return (
     <Section className={css.section}>
       <Container>
         <Title className={css.title}>{vacancy[0][lang].title}</Title>
         <div className={css.wrap}>
-          <p className={css.paragraph}>
-            <IoEllipse className={css.ioEllipse} /> Вакансія
+          <p className={clsx(css.paragraph, css.vacancyDiscr)}>
+            <IoEllipse className={css.ioEllipse} /> {translate("vacancy")}
           </p>
-          <p className={css.paragraph}>Опубліковано 01 січня 2025 року</p>
+          <p className={clsx(css.paragraph, css.joinUsDiscr)}>
+            <IoEllipse className={css.ioEllipse} /> {translate("title")}
+          </p>
+          <p className={css.paragraph}>
+            <Image
+              src={calendarIcon}
+              width={16}
+              height={16}
+              alt="calendar icon"
+              loading="lazy"
+              className={css.calendarIcon}
+            />
+            <span className={css.published}>{translate("published")}</span>
+            {translate("date")}
+            {lang === "ua" && <span className={css.published}>року</span>}
+          </p>
         </div>
         <div className={css.block}>
           <div className={css.wrapper}>
